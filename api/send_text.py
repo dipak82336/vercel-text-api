733ee5f223a2c209e78c9adc277d6c5b85115e58
dipak82336 <dipak82336@gmail.com>
2025-10-18 04:46:38 -0700

    Update send_text.py

--- api/send_text.py
+++ api/send_text.py
@@ -1,10 +1,19 @@
 from http.server import BaseHTTPRequestHandler
-from vercel_kv import kv
+from vercel_kv import KV  # 'kv' ને બદલે 'KV' इम्पोर्ट કરો
 import json
+import os  # 'os' લાઇબ્રેરી इम्पोर्ट કરો
 
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
         try:
+            # પાસવર્ડ જાતે જ os.environ માંથી લોડ કરો
+            kv = KV(
+                url=os.environ.get('KV_URL'),
+                rest_api_url=os.environ.get('KV_REST_API_URL'),
+                rest_api_token=os.environ.get('KV_REST_API_TOKEN'),
+                rest_api_read_only_token=os.environ.get('KV_REST_API_READ_ONLY_TOKEN')
+            )
+
             content_length = int(self.headers['Content-Length'])
             post_data = self.rfile.read(content_length)
             data = json.loads(post_data)
