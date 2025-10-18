3c0028b57d551597350931477897e63e438b8d84
dipak82336 <dipak82336@gmail.com>
2025-10-18 05:31:49 -0700

    Create debug_kv.py

---
+++ api/debug_kv.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler
+import json
+import os
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        keys = ["KV_REST_API_URL", "KV_REST_API_TOKEN", "KV_REST_API_READ_ONLY_TOKEN", "KV_URL", "REDIS_URL"]
+        status = {k: bool(os.getenv(k)) for k in keys}
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(status).encode('utf-8'))
