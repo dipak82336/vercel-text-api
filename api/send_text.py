430740ccafa19dbd1467e5a77543ad9795cd3b13
dipak82336 <dipak82336@gmail.com>
2025-10-18 05:09:33 -0700

    Update send_text.py

--- api/send_text.py
+++ api/send_text.py
@@ -6,13 +6,9 @@ import os
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
         try:
-            # પાસવર્ડ જાતે જ os.environ માંથી લોડ કરો
-            kv_instance = KV(
-                url=os.environ.get('KV_URL'),
-                rest_api_url=os.environ.get('KV_REST_API_URL'),
-                rest_api_token=os.environ.get('KV_REST_API_TOKEN'),
-                rest_api_read_only_token=os.environ.get('KV_REST_API_READ_ONLY_TOKEN')
-            )
+            # સાચી રીત: KV() ને કોઈ આર્ગ્યુમેન્ટની જરૂર નથી.
+            # તે આપોઆપ પાસવર્ડ શોધી લે છે.
+            kv_instance = KV()
 
             content_length = int(self.headers['Content-Length'])
             post_data = self.rfile.read(content_length)
